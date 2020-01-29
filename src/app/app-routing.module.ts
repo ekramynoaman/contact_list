32a715abc9d0e721b84691695714834a7e281925
EkramyNo3man <Ekramy.no3man@gmail.com>
2020-01-29 18:02:52 +0200

    generate 3 component Home,AddContact,NotFound && 2 services contacts,addContact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddContactComponent } from './components/add-contact/add-contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'addContact', component: AddContactComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', component: NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
